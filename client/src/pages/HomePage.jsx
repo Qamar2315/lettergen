0b54b48cf39b8e579621f0c5057f020eda3af58b
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-06-25 15:08:03 +0500

    home page modfications

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className="home">
       <header className="home-header">
         <h1>Welcome to LetterGen</h1>
-        <p>Your solution for generating personalized cover letters and emails.</p>
+        <p>
+          Your solution for generating personalized cover letters and emails.
+        </p>
       </header>
       <main>
         <section>
           <h2>Get Started</h2>
-          <p>Upload your resume and job description to receive a customized cover letter.</p>
-          <button onClick={} >Upload Resume</button>
+          <p>
+            Upload your resume and job description to receive a customized cover
+            letter.
+          </p>
+          <button
+            onClick={() => {
+              navigate("/upload-resume");
+            }}
+          >
+            Upload Resume
+          </button>
         </section>
       </main>
     </div>
