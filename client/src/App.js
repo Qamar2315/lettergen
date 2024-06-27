15c8302c25f93c1b746abeef1595408f3f916edf
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-06-27 14:18:50 +0500

    Update client/src/App.js with new component logic

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Error from "./pages/Error";
 import ResumeUploader from "./pages/ResumeUploader";
+import Navbar from "./components/Navbar";
+import AboutUs from "./components/Aboutus"; 
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar/>
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="" exact element={<Home />} />
           <Route path="/upload-resume" exact element={<ResumeUploader />} />
+          <Route path="/about" exact element={<AboutUs />} />
           <Route
             path="*"
             element={<Error message="We can't find that page." status="404" />}
